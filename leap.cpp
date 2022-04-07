e656dbcdf578b6952f31bc67a0b4929e0aa09d52
Ubuntu <ubuntu@ip-172-31-21-22.ec2.internal>
2022-04-07 20:39:20 +0000

    added code

---
+++ leap.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2022 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Huzaifa Khalid
+// Created on: April 2022
+// This function checks if the year inserted by user
+// is a leap year
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+    // This function checks if the year inserted by user
+    // is a leap year
+    int userNumber;
+    std::string userInput;
+
+    // input
+    std::cout << "Enter the number of the year (integer): ";
+    std::cin >> userInput;
+
+    // process and output
+    try {
+        userNumber = std::stoi(userInput);
+        if (userNumber % 4 == 0) {
+            if (userNumber % 100 == 0) {
+                if (userNumber % 400 == 0) {
+                    std::cout << "It is a leap year";
+                } else {
+                    std::cout << "It is not a leap year";
+                }
+            } else {
+                std::cout << "\n" << " It is a leap year"
+                            <<std::endl;
+            }
+        } else {
+            std::cout << "\n" << "It is not a leap year"
+                            <<std::endl;
+        }
+    } catch (std::invalid_argument) {
+        std::cout << "\n" << userInput << "Really? (-_-)ゞ゛pick a year pls";
+        std::cout <<""<< std::endl;
+    }
+    std::cout << "\nDone." << std::endl;
+}
